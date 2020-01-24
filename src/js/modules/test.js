39fc44358378ac188b7c3c3ee39b9e1c1943b409
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-01-23 19:37:22 -0800

    testing

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,8 +1,5 @@
 // 1. Selectors
 import { elements } from './DOMelements';
 // 2. Actions
-const test = function() {
-  console.log('test');
-};
+
 // 3. Events
-window.addEventListener('click', test);
